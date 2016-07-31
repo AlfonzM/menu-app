ef4c3d48b6ff16fc78e2e8a8ebbfd58a8f2e8d41
davybolivar <davy.bolivar@gmail.com>
2016-07-31 21:40:50 +0800

    Add Dropdown list menu on header

--- view/src/js/components/Header.js
+++ view/src/js/components/Header.js
@@ -4,7 +4,23 @@ import { Link } from "react-router";
 import SearchBox from "./SearchBox";
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false
+    };
+  }
+  handleCollapse() {
+    this.setState({ isActive: (this.state.isActive) ? false : true });
+  }
+  handleDeCollapse() {
+    this.setState({ isActive: false });
+  }
+  handleOptionClick() {
+    console.log("Option yo");
+  }
   render() {
+    const collapse = (this.state.isActive) ? 'active' : '';
     return (
       <div class="header-bar">
         <header class="header-container">
@@ -17,9 +33,31 @@ export default class Header extends React.Component {
           </div>
           <div class="header-elem-wrapper">
             <button>商品を追加する</button>
-            <button class="icon-button">
-              <i class="mdi mdi-menu-down"></i>
-            </button>
+            <div class="tool-wrapper">
+              <button className={"icon-button drop-down-button "+collapse}
+                      onClick={() => this.handleCollapse()}
+                      onBlur={() => this.handleDeCollapse()}>
+                <i class="mdi mdi-menu-down"></i>
+              </button>
+              <div class="drop-down-list">
+                <div class="option">
+                  <span>Products</span>
+                  <span class="count">1</span>
+                </div>
+                <div class="option">
+                  <span>Categories</span>
+                  <span class="count">10</span>
+                </div>
+                <div class="option">
+                  <span>Sub Categories</span>
+                  <span class="count">24</span>
+                </div>
+                <div class="divider"></div>
+                <div class="option">
+                  <span>Log Out</span>
+                </div>
+              </div>
+            </div>
           </div>
         </header>
         <div class="nav-wrapper">
